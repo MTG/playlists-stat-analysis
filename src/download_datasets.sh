94bfab6a5900ef560475ae6e2f12324456c237ce
LPorcaro <lorenzo.porcaro@gmail.com>
2019-05-27 19:15:01 +0200

    Update downloader; update license

--- src/download_datasets.sh
+++ src/download_datasets.sh
@@ -1,22 +1,20 @@
 #!/bin/sh
 
 
-
 ### AOTM ###
-# wget https://bmcfee.github.io/data/aotm2011_playlists.json.gz -P ../data/AOTM
-# gzip -d ../data/AOTM/aotm2011_playlists.json.gz
-# python playlist_pre.py -i ../data/AOTM/aotm2011_playlists.json -d AOTM
+wget https://bmcfee.github.io/data/aotm2011_playlists.json.gz -P ../data/AOTM
+gzip -d ../data/AOTM/aotm2011_playlists.json.gz
+python playlist_pre.py -i ../data/AOTM/aotm2011_playlists.json -d AOTM
 sort ../data/AOTM/playlists.tsv | uniq > ../data/AOTM/playlists_su.tsv
 mv ../data/AOTM/playlists_su.tsv ../data/AOTM/playlists.tsv
 
 ### SPOT ###
-# wget http://dbis-nowplaying.uibk.ac.at/wp-content/uploads/spotify_playlists.zip -P ../data/SPOT
-# unzip ../data/SPOT/spotify_playlists.zip -d ../data/SPOT
-# rm ../data/SPOT/spotify_playlists.zip
-# python playlist_pre.py -i ../data/SPOT/spotify_dataset.csv -d SPOT
+wget https://zenodo.org/record/2594557/files/spotify_playlists.zip -P ../data/SPOT
+unzip ../data/SPOT/spotify_playlists.zip -d ../data/SPOT
+rm ../data/SPOT/spotify_playlists.zip
+python playlist_pre.py -i ../data/SPOT/spotify_dataset.csv -d SPOT
 sort ../data/SPOT/playlists.tsv | uniq > ../data/SPOT/playlists_su.tsv
 mv ../data/SPOT/playlists_su.tsv ../data/SPOT/playlists.tsv
-
 
 ## CORN ##
 wget https://www.cs.cornell.edu/~shuochen/lme/dataset.tar.gz -P ../data/CORN
